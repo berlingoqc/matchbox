516278ce6e2b39d62511c9b40bb559bdebe7e1db
William Quintal <william95quintalwilliam@outlook.com>
2022-06-19 16:52:12 -0400

    change to git release of ggrs

--- matchbox_socket/src/ggrs_socket.rs
+++ matchbox_socket/src/ggrs_socket.rs
@@ -1,10 +1,12 @@
 use ggrs::{Message, PlayerType};
 
+use std::net::SocketAddr;
+
 use crate::WebRtcSocket;
 
 impl WebRtcSocket {
     #[must_use]
-    pub fn players(&self) -> Vec<PlayerType<String>> {
+    pub fn players(&self) -> Vec<PlayerType<SocketAddr>> {
         // needs to be consistent order across all peers
         let mut ids = self.connected_peers();
         ids.push(self.id().to_owned());
@@ -14,25 +16,27 @@ impl WebRtcSocket {
                 if id == self.id() {
                     PlayerType::Local
                 } else {
-                    PlayerType::Remote(id.to_owned())
+                    let parse_ip: SocketAddr = id.to_owned().parse().unwrap();
+                    PlayerType::Remote(parse_ip)
                 }
             })
             .collect()
     }
 }
 
-impl ggrs::NonBlockingSocket<String> for WebRtcSocket {
-    fn send_to(&mut self, msg: &Message, addr: &String) {
+impl ggrs::NonBlockingSocket<SocketAddr> for WebRtcSocket {
+    fn send_to(&mut self, msg: &Message, addr: &SocketAddr) {
         let buf = bincode::serialize(&msg).unwrap();
         let packet = buf.into_boxed_slice();
-        self.send(packet, addr);
+        self.send(packet, addr.to_string());
     }
 
-    fn receive_all_messages(&mut self) -> Vec<(String, Message)> {
+    fn receive_all_messages(&mut self) -> Vec<(SocketAddr, Message)> {
         // let fake_socket_addrs = self.fake_socket_addrs.clone();
         let mut messages = vec![];
         for (id, packet) in self.receive().into_iter() {
             let msg = bincode::deserialize(&packet).unwrap();
+            let id: SocketAddr = id.parse().unwrap();
             messages.push((id, msg));
         }
         messages
